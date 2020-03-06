4f500d4b0062da0d4ce4819be2f73f38dd7285d1
Celeste Thomas <celestesmthomas@gmail.com>
2020-03-06 00:39:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+require 'benchmark'
+
+
+def number(array)
+  primes = (0..array).to_a
+  primes[0] = primes[1] = nil
+  counter = 0
+  primes.each do |p|
+    next unless p
+    break if p*p > array
+    counter += 1
+    (p*p).step(array,p) { |m| primes[m] = nil }
+  end
+
+
+  puts "Solved for #{array} in #{counter} steps."
+  primes.compact
+end
+
+def prime?(num)
+  number(num).include?(num)
+end
+
+puts prime?(105557)
